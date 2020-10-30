30f3294bd055181a8954c4da5af5760481ca4060
Edwin Spee <Edwin.Spee@epsig.nl>
2020-10-30 11:32:41 +0100

    Issue #31 white space changes

--- code/shob.pl
+++ code/shob.pl
@@ -362,173 +362,173 @@ sub sport_init()
 sub shob_main_loop()
 {# (c) Edwin Spee
 
- my @start_cpu = times;
- my $start_wt = gettimeofday;
+  my @start_cpu = times;
+  my $start_wt = gettimeofday;
 
- init_host_id();
- my $hostid = get_host_id();
- init_webdir($hostid);
+  init_host_id();
+  my $hostid = get_host_id();
+  init_webdir($hostid);
 
- my $is_remote = ($hostid eq 'remote' or $hostid eq 'epsig' or $hostid eq 'wh_epsig');
- my $lv = $is_remote ? 'n' : 'y';
- my $nh = $lv;
- my $defaults = $is_remote ? 1 : 0;
- my $parts = 0;
+  my $is_remote = ($hostid eq 'remote' or $hostid eq 'epsig' or $hostid eq 'wh_epsig');
+  my $lv = $is_remote ? 'n' : 'y';
+  my $nh = $lv;
+  my $defaults = $is_remote ? 1 : 0;
+  my $parts = 0;
 
- my $opt;
- my $cmd = '';
+  my $opt;
+  my $cmd = '';
 
- foreach my $arg (@ARGV)
- {
-  # handle --mkdir
-  if ($arg =~ m/--m/iso)
+  foreach my $arg (@ARGV)
   {
-   no_prompt_mkdir();
-  }
-  # handle --dir=string
-  if ($arg =~ m/--d.*=(.*)/iso)
-  {
-   my $dir = $1;
-   print "Output dir = $dir\n";
-   overrule_webdir($dir);
-  }
- }
-
- if (scalar @ARGV)
- {
-  my $command = $ARGV[scalar @ARGV - 1];
-  if ($command =~ m/-(d.*)/iso)
-  {
-   $cmd = lc($1);
-  }
-  if ($command =~ m/-he/iso)
-  {
-   #print_help_page();
-   system('perldoc', 'shob.pl');
-   exit 0;
-  }
-  elsif ($command =~ m/-hi/iso)
-  {
-   print "\nRebuilding old website!\n\n";
-   print_version();
-   set_history_on;
-   $cmd = 'd3';
-  }
-  elsif ($command =~ m/-h/iso)
-  {
-   print "-h is ambitious; use -help or -his:\n\n";
-   #print_help_page();
-   system('perldoc', 'shob.pl');
-   exit 0;
-  }
-  elsif ($command =~ m/-c/iso)
-  {
-   print "OK\n"; # syntax ok.
-   exit 0;
-  }
-  elsif ($command =~ m/-v/iso)
-  {
-   print_version();
-   exit 0;
-  }
-  elsif ($command =~ m/-e/iso or $command =~ m/-di/iso)
-  {
-    if ($withAdresses)
+    # handle --mkdir
+    if ($arg =~ m/--m/iso)
     {
-      require Admin::Adressen_data2html;
-      Admin::Adressen_data2html->import;
-
-      if ($command =~ m/-e/iso)
-      {
-        edit_addresses(0);
-      }
-      elsif ($command =~ m/-di/iso)
-      {
-        diff_adres();
-      }
+      no_prompt_mkdir();
     }
-    exit 0;
+    # handle --dir=string
+    if ($arg =~ m/--d.*=(.*)/iso)
+    {
+      my $dir = $1;
+      print "Output dir = $dir\n";
+      overrule_webdir($dir);
+    }
   }
-  elsif ($ARGV[scalar @ARGV - 2] =~ m/-s/iso)
+
+  if (scalar @ARGV)
   {
-   zoek_term($ARGV[scalar @ARGV - 1], File::Spec->curdir, 0);
-   exit 0;
+    my $command = $ARGV[scalar @ARGV - 1];
+    if ($command =~ m/-(d.*)/iso)
+    {
+      $cmd = lc($1);
+    }
+    if ($command =~ m/-he/iso)
+    {
+      #print_help_page();
+      system('perldoc', 'shob.pl');
+      exit 0;
+    }
+    elsif ($command =~ m/-hi/iso)
+    {
+      print "\nRebuilding old website!\n\n";
+      print_version();
+      set_history_on;
+      $cmd = 'd3';
+    }
+    elsif ($command =~ m/-h/iso)
+    {
+      print "-h is ambitious; use -help or -his:\n\n";
+      #print_help_page();
+      system('perldoc', 'shob.pl');
+      exit 0;
+    }
+    elsif ($command =~ m/-c/iso)
+    {
+      print "OK\n"; # syntax ok.
+      exit 0;
+    }
+    elsif ($command =~ m/-v/iso)
+    {
+      print_version();
+      exit 0;
+    }
+    elsif ($command =~ m/-e/iso or $command =~ m/-di/iso)
+    {
+      if ($withAdresses)
+      {
+        require Admin::Adressen_data2html;
+        Admin::Adressen_data2html->import;
+
+        if ($command =~ m/-e/iso)
+        {
+          edit_addresses(0);
+        }
+        elsif ($command =~ m/-di/iso)
+        {
+          diff_adres();
+        }
+      }
+      exit 0;
+    }
+    elsif ($ARGV[scalar @ARGV - 2] =~ m/-s/iso)
+    {
+      zoek_term($ARGV[scalar @ARGV - 1], File::Spec->curdir, 0);
+      exit 0;
+    }
   }
- }
 
- if ($cmd eq '')
- {
-  print 'Actie: defaults (d), make(1), print verschillen (2), make met confirm (3=d/dl/d3): ';
-  $cmd = lees_regel;
- }
-
- sport_init();
-
- if ($cmd eq 'd' or $cmd eq 'dl' or $cmd =~ m/^d\d$/iso)
- {
-  $defaults = 2;
-  $opt = 3;
-
-  if ($cmd eq 'dl')
+  if ($cmd eq '')
   {
-   $parts = 1;
+    print 'Actie: defaults (d), make(1), print verschillen (2), make met confirm (3=d/dl/d3): ';
+    $cmd = lees_regel;
   }
-  elsif ($cmd =~ m/d(\d)/iso)
+
+  sport_init();
+
+  if ($cmd eq 'd' or $cmd eq 'dl' or $cmd =~ m/^d\d$/iso)
   {
-   $parts = $1 - 1;
-  }
- }
- elsif ($cmd =~ m/q/iso or $cmd =~ m/x/iso)
- {
-  print "\nQuit.\n\n";
-  exit 1;
- }
- else
- {
-  $opt = $cmd;
- }
+    $defaults = 2;
+    $opt = 3;
 
- if ($defaults <= 1)
- {
-  print "Welke Onderdelen:\n";
-  print '(vrijwel-)alles (0=d), lopend(1), kj(2) of test/nieuw (3): ';
-  $parts = lees_regel;
-  if ($parts =~ m/d/iso)
+    if ($cmd eq 'dl')
+    {
+      $parts = 1;
+    }
+    elsif ($cmd =~ m/d(\d)/iso)
+    {
+      $parts = $1 - 1;
+    }
+  }
+  elsif ($cmd =~ m/q/iso or $cmd =~ m/x/iso)
   {
-   $defaults = 2;
-   $parts = 0;
+    print "\nQuit.\n\n";
+    exit 1;
   }
- }
+  else
+  {
+    $opt = $cmd;
+  }
 
- if ($defaults == 0 and $parts != 2)
- {
-  print 'Locale versie: (jyn) ';
-  $lv = lees_regel;
-  print 'Nette html (jyn) ';
-  $nh = lees_regel;
- }
+  if ($defaults <= 1)
+  {
+    print "Welke Onderdelen:\n";
+    print '(vrijwel-)alles (0=d), lopend(1), kj(2) of test/nieuw (3): ';
+    $parts = lees_regel;
+    if ($parts =~ m/d/iso)
+    {
+      $defaults = 2;
+      $parts = 0;
+    }
+  }
 
- init_settings($nh, $opt, 0, $lv);
- init_web_index($lv =~ m/n/iso ? 0 : 1);
- my $parts2 = ($parts < 2 ? 2 - $parts : $parts+1);
- handle_new_style_files($opt, $lv, $parts2);
+  if ($defaults == 0 and $parts != 2)
+  {
+    print 'Locale versie: (jyn) ';
+    $lv = lees_regel;
+    print 'Nette html (jyn) ';
+    $nh = lees_regel;
+  }
 
- my @stop_cpu = times;
- my $stop_wt = gettimeofday;
- my $diff_wt = $stop_wt - $start_wt;
- printf "user:     %.2f\n", $stop_cpu[0] - $start_cpu[0];
- my $get_waiting_time = get_waiting_time;
- if ($get_waiting_time > 0.0)
- {
-  printf "walltime: %.2f; waiting: %.2f; walltime - waiting: %.2f\n",
-   $diff_wt , $get_waiting_time,
-   $diff_wt - $get_waiting_time;
- }
- else
- {
-  printf "walltime: %.2f\n", $diff_wt;
- }
- printf "aantal keer filter in sort: %i\n", get_counter();
+  init_settings($nh, $opt, 0, $lv);
+  init_web_index($lv =~ m/n/iso ? 0 : 1);
+  my $parts2 = ($parts < 2 ? 2 - $parts : $parts+1);
+  handle_new_style_files($opt, $lv, $parts2);
+
+  my @stop_cpu = times;
+  my $stop_wt = gettimeofday;
+  my $diff_wt = $stop_wt - $start_wt;
+  printf "user:     %.2f\n", $stop_cpu[0] - $start_cpu[0];
+  my $get_waiting_time = get_waiting_time;
+  if ($get_waiting_time > 0.0)
+  {
+    printf "walltime: %.2f; waiting: %.2f; walltime - waiting: %.2f\n",
+      $diff_wt , $get_waiting_time,
+      $diff_wt - $get_waiting_time;
+  }
+  else
+  {
+    printf "walltime: %.2f\n", $diff_wt;
+  }
+  printf "aantal keer filter in sort: %i\n", get_counter();
 }
 
 shob_main_loop();
