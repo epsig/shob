724ab47af0b0a382209bb33bd6208ee94b75a71d
Edwin Spee <Edwin.Spee@epsig.nl>
2020-12-12 11:52:45 +0100

    Issue #2 convert OS 1994 to csv and use it

--- code/ShobTest.pm
+++ code/ShobTest.pm
@@ -21,83 +21,9 @@ $VERSION = '20.1';
  #========================================================================
 );
 
-my $yr = 1998;
-
-sub dumpOS2csv($)
-{
-  my $OS = shift;
-
-  my @sxs =  ('H', 'H', 'H', 'H', 'H', 'D', 'D', 'D', 'D', 'D', 'H', 'D', 'H', 'D');
-
-  my @distances = ('500m', '1000m', '1500m', '5km', '10km',
-   '500m', '1000m', '1500m', '3km', '5km',
-   'teampursuit', 'teampursuit', 'massaStart', 'massaStart');
-
-  open (OUT, ">Sport_Data/schaatsen/OS_$yr.csv") or die "can't open OS_$yr: $!.\n";
-
-  print OUT "DH,distance,ranking,name,team,result,remark\n";
-
-  my @OSarr = @$OS;
-  my $ii = -1;
-  foreach my $OSpart (@OSarr)
-  {
-    $ii++;
-    my $size = scalar @$OSpart;
-    foreach my $result (@$OSpart)
-    {
-      if (scalar @$result == 0) {next;}
-      print OUT $sxs[$ii], ',', $distances[$ii], ',';
-      my $jj = -1;
-      foreach my $field (@$result)
-      {
-        $jj++;
-        if (ref $field eq 'ARRAY')
-        {
-          my $kk = -1;
-          foreach my $subfield (@$field)
-          {
-            $kk++;
-            if (scalar @$result == 4 && $kk == 1)
-            {
-              print OUT " $subfield;";
-            }
-            else
-            {
-              if ($kk > 0) {print OUT ',';}
-              $subfield =~ s/,/;/g;
-              print OUT "$subfield";
-              if ($jj == 1) {print OUT ',';}
-            }
-          }
-        }
-        else
-        {
-          my $char = ',';
-          $char = ';' if (scalar @$result == 4 && $jj == 2); # 500m: 2x
-          print OUT $field,$char;
-          if ($jj == 1) {print OUT ',';}
-        }
-      }
-      print OUT "\n";
-    }
-  }
-  close(OUT) or die "can't close: $!.\n";
-}
-
 sub test_something()
 {
-  # print "place holder for tmp/test function.\n";
-
-  use Sport_Collector::OS_Schaatsen;
-  use Sport_Collector::OS_Funcs;
-  use Sport_Collector::Teams;
-
-  initTeams();
-  read_schaatsers();
-
-  my $txt = eval("get_OS$yr()");
-
-  dumpOS2csv($OS);
+  print "place holder for tmp/test function.\n";
 }
 
 1;
--- code/Sport_Collector/OS_Schaatsen.pm
+++ code/Sport_Collector/OS_Schaatsen.pm
@@ -28,194 +28,8 @@ $VERSION = '18.1';
  '&get_OS2010',
  '&get_OS2014',
  '&get_OS2018',
- '$OS',
  #========================================================================
 );
-
-our $OS;
-
-sub get_OS1994()
-{# (c) Edwin Spee
-
- my $H500m_1994 = [ [],
-[ 1, 'RUglb', [36.33, 'OR']],
-[ 2, 'RUklv', 36.39],
-[ 3, 'JPhor', 36.53],
-[ 4, 'CNhbl', 36.54],
-[ 5, 'JPsm2', 36.60],
-[ 6, 'JPino', 36.63],
-[ 7, 'NOnjs', 36.66],
-[ 8, 'USjns', 36.68],
-[ 9, 'JPmyb', 36.72],
-[10, 'RUzjl', 36.73],
-[21, 'NLvld', 37.45],
-[24, 'NLlof', 37.52],
-[31, 'NLvls', 37.94],
-[37, 'NLsrd', 38.33],
-#WR:35'76" OR:36'33" NR:36'76"
-#Jansen    Goloebev  Ykema
-];
-
- my $H1000m_1994 = [ [],
-[ 1, 'USjns', [72.43, 'WR']],
-[ 2, 'RUzjl', 72.72],
-[ 3, 'RUklv', 72.85],
-[ 4, 'CNhbl', 73.47],
-[ 5, 'CAsbc', 73.56],
-[ 6, 'CAkll', 73.67],
-[ 7, 'NOst2', 73.74],
-[ 8, 'JPino', 73.75],
-[ 9, 'NLvld', 73.81],
-[10, 'CAsct', 73.82],
-[15, 'NLvls', 74.29],
-[22, 'NLlof', 75.12],
-[24, 'NLsrd', 75.19],
-#WR:1.12'43" OR:1.12'43" NR:1.13'10"
-#Jansen      Jansen      Van Velde
-];
-
- my $H1500m_1994 = [ [],
-[ 1, 'NOkss', [60+51.29, 'WR']],
-[ 2, 'NLrts', 60+51.99],
-[ 3, 'NLznd', 60+52.38],
-[ 4, 'NOsnd', 60+53.13],
-[ 5, 'RUanf', 60+53.16],
-[ 6, 'DEadb', 60+53.50],
-[ 7, 'CAmrs', 60+53.56],
-[ 8, 'NLhrs', 60+53.59],
-[ 9, 'NLstr', 60+53.70],
-[10, 'UAsjl', 60+54.28],
-#WR:1.51'29" OR:1.51'29" NR:1.51'60"
-#Koss        Koss        Ritsma
-];
-
- my $H5km_1994 = [ [],
-[ 1, 'NOkss', [6*60+34.96, 'WR']],
-[ 2, 'NOstl', 6*60+42.68],
-[ 3, 'NLrts', 6*60+43.94],
-[ 4, 'NLznd', 6*60+44.58],
-[ 5, 'NLbvl', 6*60+49.00],
-[ 6, 'JPitk', 6*60+49.36],
-[ 7, 'PLrdk', 6*60+50.40],
-[ 8, 'DEdtt', 6*60+52.27],
-[ 9, 'AThds', 6*60+53.02],
-[10, 'ATemn', 6*60+53.18],
-[11, 'RUanf', 6*60+53.23],
-[12, 'SEsch', 6*60+53.39],
-[13, 'JPsat', 6*60+54.83],
-[14, 'SEbng', 6*60+57.37],
-#WR:6.34'96" OR:6.34'96" NR:6.39'40"
-#Koss        Koss        Veldkamp
-];
-
- my $H10km_1994 = [ [],
-[ 1, 'NOkss', [13*60+30.55, 'WR']],
-[ 2, 'NOstl', 13*60+49.25],
-[ 3, 'NLbvl', 13*60+56.73],
-[ 4, 'NLznd', 13*60+58.25],
-[ 5, 'PLrdk', 14*60+ 3.84],
-[ 6, 'DEdtt', 14*60+ 4.33],
-[ 7, 'NLrts', 14*60+ 9.28],
-[ 8, 'SEsch', 14*60+10.15],
-[ 9, 'AThds', 14*60+12.09],
-[10, 'ATemn', 14*60+15.14],
-#WR: 13.30'55" OR: 13.30'55" NR: 13.46'34"
-#Koss          Koss          Veldkamp
-];
-
- my $D500m_1994 = [ [],
-[ 1, 'USblr', 39.25],
-[ 2, 'CAach', 39.61],
-[ 3, 'DEsch', 39.70],
-[ 4, 'CNrhx', 39.71],
-[ 5, 'KRshy', 39.92],
-[ 6, 'DEgbr', 39.95],
-[ 7, 'RUbjr', 40.17],
-[ 8, 'NOhst', 40.20],
-[ 9, 'CNjng', 40.23],
-[10, 'JPshm', 40.26],
-[11, 'CNyng', 40.37],
-[12, 'DEhck', 40.38],
-[19, 'NLaft', 41.01],
-#WR:39'10"   OR:39'10"   NR:40'34"
-#Blair       Blair       Aaftink
-];
-
- my $D1000m_1994 = [ [],
-[ 1, 'USblr', 60+18.74],
-[ 2, 'DEbar', 60+20.12],
-[ 3, 'CNqby', 60+20.22],
-[ 4, 'DEsch', 60+20.25],
-[ 5, 'DEgbr', 60+20.32],
-[ 6, 'JPksn', 60+20.37],
-[ 7, 'AThnd', 60+20.42],
-[ 8, 'CAach', 60+20.72],
-[ 9, 'RUrvl', 60+20.82],
-[10, 'RUplt', 60+20.84],
-[11, 'RUfdt', 60+20.89],
-[12, 'DEhck', 60+20.93],
-[14, 'NLthm', [60+20.94, 'NR']],
-[20, 'NLaft', 60+22.16],
-#WR:1.17'65"  OR:1.17'65"  NR:1.20'94"
-#Rothenburger Rothenburger Thomas
-];
-
- my $D1500m_1994 = [ [],
-[ 1, 'AThnd', 2*60+2.19],
-[ 2, 'RUfdt', 2*60+2.69],
-[ 3, 'DEnmn', 2*60+3.41],
-[ 4, 'USblr', 2*60+3.44],
-[ 5, 'NLthm', 2*60+3.70],
-[ 6, 'RUbsj', 2*60+3.99],
-[ 7, 'RUplt', 2*60+4.00],
-[ 8, 'ROdsc', 2*60+4.02],
-[ 9, 'JPhsh', 2*60+4.98],
-[10, 'NLtdj', 2*60+5.18],
-[11, 'DEbar', 2*60+5.97],
-[12, 'ITblc', 2*60+5.99],
-[13, 'JPksn', 2*60+6.20],
-[14, 'DEudb', 2*60+6.40],
-[22, 'NLzls', 2*60+8.49],
-#WR:1.59'30" OR:2.00'68" NR:2.00'68"
-#Kania       Van Gennip  Van Gennip
-];
-
- my $D3km_1994 = [ [],
-[ 1, 'RUbsj', 4*60+17.43],
-[ 2, 'AThnd', 4*60+18.14],
-[ 3, 'DEpch', 4*60+18.34],
-[ 4, 'KZprk', 4*60+19.33],
-[ 5, 'NLthm', 4*60+19.82],
-[ 6, 'JPhsh', 4*60+21.07],
-[ 7, 'JPymm', 4*60+22.37],
-[ 8, 'ROdsc', 4*60+22.42],
-[ 9, 'NLzls', 4*60+23.42],
-[10, 'JPogs', 4*60+25.27],
-[11, 'NLtdj', 4*60+25.88],
-[12, 'RUtrp', 4*60+27.82],
-[13, 'ATant', 4*60+27.91],
-#Niemann viel; Belci gediskwalificeerd.
-#WR:4.10'80" OR:4.11'94" NR:4.11'94"
-#Niemann     Van Gennip  Van Gennip
-];
-
- my $D5km_1994 = [ [],
-[ 1, 'DEpch', 7*60+14.37],
-[ 2, 'DEnmn', 7*60+14.88],
-[ 3, 'JPymm', 7*60+19.68],
-[ 7, 'NLzls', 7*60+29.42],
-[10, 'NLthm', 7*60+32.39],
-[11, 'NLtdj', 7*60+36.07],
-];
-
- my $out = format_os(
-  [$H500m_1994, $H1000m_1994, $H1500m_1994, $H5km_1994, $H10km_1994,
-   $D500m_1994, $D1000m_1994, $D1500m_1994, $D3km_1994, $D5km_1994]);
-
- my $title = 'Schaatsen OS 1994 Lillehammer; Vikingskip, Hamar';
- return maintxt2htmlpage(OSTopMenu(1994) . $out, $title, 'title2h1',
-  20050319, {type1 => 'std_menu'});
-}
 
 sub get_one_distance($$$)
 {
@@ -295,6 +109,20 @@ sub get_all_distances($)
   return \@games;
 }
 
+sub get_OS1994()
+{# (c) Edwin Spee
+
+  my $cvsFile = File::Spec->catdir($csv_dir, 'schaatsen', 'OS_1994.csv');
+  my $OS1994 = get_all_distances($cvsFile);
+
+  my $out = format_os($OS1994);
+
+  my $title = 'Schaatsen OS 1994 Lillehammer; Vikingskip, Hamar';
+
+  return maintxt2htmlpage(OSTopMenu(1994) . $out, $title, 'title2h1',
+    20050319, {type1 => 'std_menu'});
+}
+
 sub get_OS1998()
 {# (c) Edwin Spee
 
@@ -369,4 +197,3 @@ sub get_OS2018
 }
 
 return 1;
-
