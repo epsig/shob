eb01eddf6c01d72757d8ce10d5df74a218255bab
Edwin Spee <Edwin.Spee@epsig.nl>
2021-08-09 11:12:40 +0200

    #47 fix for empty beker results

--- code/Sport_Functions/BekerReaders.pm
+++ code/Sport_Functions/BekerReaders.pm
@@ -57,29 +57,35 @@ sub read_beker_csv($$$)
   my $content = read_csv_with_header($filein, $subdir);
 
   my $sc = read_beker_part('supercup', 1, "Johan Cruijff schaal $year", 5, $content);
-  my $r2 = read_beker_part('r2', 1, 'Tweede ronde', 5, $content);
-  my $f8 = read_beker_part('8f', 1, 'achtste-finales KNVB-beker', 5, $content);
-  my $f4 = read_beker_part('4f', 1, 'kwart-finale KNVB-beker', 5, $content);
-  my $f2 = read_beker_part('2f', 1, 'halve finale KNVB-beker', 5, $content);
-  my $f = read_beker_part('f', 1, 'finale KNVB-beker', 5, $content);
-  my $f34 = read_beker_part('f34', 1, 'troost-finale KNVB-beker', 5, $content);
+
+  my %bekerKeys;
+  $bekerKeys{'r2'} = ['round2',       'Tweede ronde'];
+  $bekerKeys{'8f'} = ['round_of_16',  'achtste-finales KNVB-beker'];
+  $bekerKeys{'4f'} = ['quarterfinal', 'kwart-finale KNVB-beker'];
+  $bekerKeys{'2f'} = ['semifinal',    'halve finale KNVB-beker'];
+  $bekerKeys{'f'}  = ['final',        'finale KNVB-beker'];
+  $bekerKeys{'f34'}= ['u34',          'troost-finale KNVB-beker'];
+
+  my $beker;
+  foreach my $bkr ('r2', '8f', '4f', '2f', 'f', 'f34')
+  {
+    my $title = $bekerKeys{$bkr}[1];
+    my $key   = $bekerKeys{$bkr}[0];
+    my $result = read_beker_part($bkr, 1, $title, 5, $content);
+    if (defined $result)
+    {
+      $beker->{$key} = $result;
+    }
+  }
 
   my $opm = $all_remarks->{eredivisie}->get_ml(yr2szn($year), 'beker_opm', 1);
+  $beker->{beker_opm} = $opm if ($opm ne '');
 
-  my $beker = {
-    extra => { supercup => $sc },
-    beker => {
-      round2 => $r2,
-      round_of_16 => $f8,
-      quarterfinal => $f4,
-      semifinal => $f2,
-      final => $f,
-      u34 => $f34,
-      beker_opm => $opm
-    }
-  };
+  my $out;
+  $out->{extra}{supercup} = $sc    if (defined $sc);
+  $out->{beker}           = $beker if (defined $beker);
 
-  return $beker;
+  return $out;
 }
 
 return 1;
