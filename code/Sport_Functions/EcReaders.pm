931a9717abb67bdc3b4f0b5e8ece0ac5b2e43e37
Edwin Spee <Edwin.Spee@epsig.nl>
2023-02-18 21:20:17 +0100

    #51 update

--- code/Sport_Functions/EcReaders.pm
+++ code/Sport_Functions/EcReaders.pm
@@ -114,12 +114,13 @@ sub read_ec_csv($$)
     if ( ! leagueIsActive($content, $league)) {next;}
 
     my %finals;
+    $finals{'tr'} = ['tr', "extra round $shortname"];
     $finals{'8f'} = ['round_of_16', "8-ste finale $shortname"];
     $finals{'4f'} = ['quarterfinal', "kwart finale $shortname"];
     $finals{'2f'} = ['semifinal', "halve finale $shortname"];
     $finals{'f'}  = ['final', "finale $shortname"];
 
-    foreach my $l ('8f', '4f', '2f', 'f')
+    foreach my $l ('tr', '8f', '4f', '2f', 'f')
     {
       my $title = $finals{$l}[1];
       my $k     = $finals{$l}[0];
