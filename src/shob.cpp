de10ada5fb712629ae1bb9bc1ab2774ca8552280
Edwin Spee <edwin.spee@deltares.nl>
2025-05-30 17:38:25 +0200

    switch in main for nl / ec

--- src/shob.cpp
+++ src/shob.cpp
@@ -1,23 +1,34 @@
 
 #include <iostream>
 
+#include "shob.pages/format_ec_factory.h"
 #include "shob.pages/format_nl_factory.h"
 
 int main(int argc, char* argv[])
 {
-    if (argc < 2)
+    if (argc < 3)
     {
-        std::cout << "usage: " << argv[0] << " season" << std::endl;
-        std::cout << "example season : 2022-2023." << std::endl;
+        std::cout << "usage: " << argv[0] << " type season" << std::endl;
+        std::cout << "example type season : nl 2022-2023." << std::endl;
+        std::cout << "                 or : ec 2012-2013." << std::endl;
         std::cout << "must be run in the 'data' folder." << std::endl;
         return -1;
     }
-    std::string season = argv[1];
+    std::string type = argv[1];
+    std::string season = argv[2];
 
     try
     {
-        auto fmt_nl = shob::pages::format_nl_factory::build("sport");
-        fmt_nl.get_season_stdout(season);
+        if (type == "nl")
+        {
+            auto fmt_nl = shob::pages::format_nl_factory::build("sport");
+            fmt_nl.get_season_stdout(season);
+        }
+        else if (type == "ec")
+        {
+            auto fmt_ec = shob::pages::format_ec_factory::build("sport");
+            fmt_ec.get_season_stdout(season);
+        }
     }
     catch (const std::exception& e)
     {
