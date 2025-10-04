a4e248c53fbec4cf343c9132679cb5ab5c5b12b3
Edwin Spee <edwin.spee@deltares.nl>
2025-10-04 16:08:35 +0200

    format_ec now in exported pages folder

--- src/shob.cpp
+++ src/shob.cpp
@@ -1,4 +1,5 @@
 
+#include <filesystem>
 #include <iostream>
 
 #include "shob.general/dateFactory.h"
@@ -33,23 +34,27 @@ int main(int argc, char* argv[])
         auto fmt_nl = format_nl_factory::build("sport");
         auto settings = shob::html::settings();
         settings.dateFormatShort = false;
-        auto settings2 = shob::html::settings();
-        settings2.dateFormatShort = false;
-        settings2.isCompatible = true;
         auto fmt_ec = format_ec_factory::build("sport", settings);
-        auto fmt_ec2 = format_ec_factory::build("sport", settings2);
         for (int year = firstYear; year <= lastYear; year++)
         {
             const auto season = shob::general::season(year);
-            fmt_nl.get_season_to_file(season, "../pages_new/sport_voetbal_nl_" + season.to_part_filename() + ".html");
+            if ( std::filesystem::is_directory("../pages_new/"))
+            {
+                fmt_nl.get_season_to_file(season, "../pages_new/sport_voetbal_nl_" + season.to_part_filename() + ".html");
+            }
             if (year >= 1994)
             {
-                fmt_ec.get_season_to_file(season, "../pages_new/sport_voetbal_europacup_" + season.to_part_filename() + ".html");
-                fmt_ec2.get_season_to_file(season, "../pages_compatible/sport_voetbal_europacup_" + season.to_part_filename() + ".html");
+                fmt_ec.get_season_to_file(season, "../pages/sport_voetbal_europacup_" + season.to_part_filename() + ".html");
             }
         }
-        shob::html::updateIfDifferent::update("../code/test/epsig.css", "../pages_new/epsig.css");
-        shob::html::updateIfDifferent::update("../code/test/epsig.css", "../pages_compatible/epsig.css");
+        if (std::filesystem::is_directory("../pages_new/"))
+        {
+            shob::html::updateIfDifferent::update("../code/test/epsig.css", "../pages_new/epsig.css");
+        }
+        if (std::filesystem::is_directory("../pages_compatible/"))
+        {
+            shob::html::updateIfDifferent::update("../code/test/epsig.css", "../pages_compatible/epsig.css");
+        }
     }
     catch (const std::exception& e)
     {
