1b5a316bcb432f37a88367d0efeabab03795783e
Edwin Spee <edwin.spee@deltares.nl>
2025-10-21 11:09:02 +0200

    lastDate only matches that have a result

--- src/shob.football/footballCompetition.cpp
+++ src/shob.football/footballCompetition.cpp
@@ -204,7 +204,10 @@ namespace shob::football
         int date = 19000101;
         for (const auto& match : matches)
         {
-            date = std::max(date, match.dd->toInt());
+            if (match.result != "-")
+            {
+                date = std::max(date, match.dd->toInt());
+            }
         }
         auto dd = itdate(date);
         return dd;
