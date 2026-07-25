3540e51cf591097b5cd13ab33feba40d51e3a2d7
Edwin Spee <edwin.spee@deltares.nl>
2026-07-25 14:02:21 +0200

    #72 fix test after last minute changes

--- src/shob.pages.test/TestFormatEkWk.cpp
+++ src/shob.pages.test/TestFormatEkWk.cpp
@@ -20,8 +20,8 @@ namespace shob::pages::test
         const auto format_ek_wk = FormatEkWkFactory::build(data_folder, settings);
         const auto lines = format_ek_wk.getPages(2022);
         ASSERT_EQ(lines.data.size(), 233);
-        EXPECT_GE(lines.findString("Scheidsrechter:  Ghorbal (DZ) . </br>"), 0);
-        EXPECT_GE(lines.findString("81 min 3-1 Denzel Dumfries<br/>"), 0);
+        EXPECT_GE(lines.findString("Scheidsrechter: Ghorbal (DZ). </br>"), 0);
+        EXPECT_GE(lines.findString("81' 3-1 Denzel Dumfries<br/>"), 0);
         EXPECT_GE(lines.findString("Na 64 wedstrijden: 3.45 miljoen toeschouwers; gemiddeld = 54 duizend."), 0);
         EXPECT_GE(lines.findString("<tr><td>&nbsp;Spanje - Costa Rica&nbsp;</td><td class=c>&nbsp;7-0&nbsp;</td><td>&nbsp;Spanje - Costa Rica&nbsp;</td><td class=c>&nbsp;7-0&nbsp;</td><td>&nbsp;Engeland - Iran&nbsp;</td><td class=c>&nbsp;6-2&nbsp;</td></tr>"), 0);
         EXPECT_GE(lines.findString("Strafschoppenserie: 3-4"), 0);
