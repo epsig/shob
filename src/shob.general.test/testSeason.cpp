32c0676b63c59b5d1c457b9386b6256177462e66
Edwin Spee <edwin.spee@deltares.nl>
2026-07-25 11:11:19 +0200

    avoid failing test when adding a EC season

--- src/shob.general.test/testSeason.cpp
+++ src/shob.general.test/testSeason.cpp
@@ -7,14 +7,14 @@ namespace shob::general::test
 {
     void testSeason::test1()
     {
-        const auto season = general::Season(2020);
+        const auto season = Season(2020);
         const auto str = season.toString();
         ASSERT_EQ(str, "2020-2021");
     }
 
     void testSeason::test2()
     {
-        const auto season = general::Season(2020);
+        const auto season = Season(2020);
         const auto str = season.toPartFilename();
         ASSERT_EQ(str, "2020_2021");
     }
