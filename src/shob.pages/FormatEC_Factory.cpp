fea52da24aa229e7f20d4f92bd9cc58005007add
Edwin Spee <edwin.spee@deltares.nl>
2026-07-31 11:38:57 +0200

    #104 add sorting to EC factory

--- src/shob.pages/FormatEC_Factory.cpp
+++ src/shob.pages/FormatEC_Factory.cpp
@@ -1,10 +1,20 @@
 
 #include "FormatEC_Factory.h"
+
+#include <algorithm>
+
 #include "TopMenu.h"
 #include "../shob.general/glob.h"
 
 namespace shob::pages
 {
+    bool FormatEC_Factory::cmpFunc(const std::string& a, const std::string& b)
+    {
+        auto num1 = a.substr(10, 4);
+        auto num2 = b.substr(10, 4);
+        return num1 < num2;
+    }
+
     FormatEC FormatEC_Factory::build(const std::string& dataFolder, const html::settings& settings)
     {
         auto remarks = readers::csvAllSeasonsReader();
@@ -19,6 +29,7 @@ namespace shob::pages
         teams.AddLandCodes(national_teams);
 
         auto archive = general::glob::list(dataFolder + "/europacup", "europacup_[0-9].*csv");
+        std::sort(archive.begin(), archive.end(), cmpFunc);
         auto menu = TopMenu(archive, 'K');
 
         auto leagueNames = football::leagueNames(dataFolder + "/europacup/league_names.csv", settings.isCompatible);
--- src/shob.pages/FormatEC_Factory.h
+++ src/shob.pages/FormatEC_Factory.h
@@ -8,6 +8,7 @@ namespace shob::pages
     class FormatEC_Factory
     {
     public:
+        static bool cmpFunc(const std::string& a, const std::string& b);
         static FormatEC build(const std::string& dataFolder, const html::settings& settings);
     };
 }
