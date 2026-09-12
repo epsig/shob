535902e659b5b864f8018c017af753f8ff454d16
Edwin Spee <edwin@epsig.nl>
2026-09-12 20:07:06 +0200

    add rebuildOverzicht

--- src/shob.pages/FormatBookmarks.cpp
+++ src/shob.pages/FormatBookmarks.cpp
@@ -146,7 +146,7 @@ namespace shob::pages
         content.addContentReversed(os_links);
         content.addContent("</li> <li>Zie verder: <a href=\"bookmarks_sport.html\">sport links</a> </li> </ul>");
 
-        auto hb = HeadBottomInput(20260906); // TODO: use current dd
+        auto hb = HeadBottomInput(last_dd);
         hb.title = "Sportpagina's op www.epsig.nl";
         hb.css = StyleSheetType::SeparateFile;
         hb.copyTitleToH1 = false;
@@ -154,5 +154,67 @@ namespace shob::pages
         auto pageContent = HeadBottom::getPage(hb);
 
         updateIfDifferent::update("../pages/sport.html", pageContent);
+    }
+
+    void FormatBookmarks::rebuildOverzicht()
+    {
+        const auto os = OwnSportPages::getOlympicIceSkating();
+        const auto ekwk = OwnSportPages::getEkWkSoccer();
+        const auto ekwk_D = OwnSportPages::getEkWkSoccerWoman();
+        const auto eredivisie = OwnSportPages::getDutchSoccer(true);
+        const auto europacup = OwnSportPages::getEuropacupSoccer(true);
+
+        auto os_links = os.printAll();
+        auto ekwk_links = ekwk.printAll();
+        auto ekwk_D_links = ekwk_D.printAll();
+        auto eredivisie_links = eredivisie.printAll();
+        auto europacup_links = europacup.printAll();
+
+        AddPunctuation(os_links);
+        AddPunctuation(ekwk_links);
+        AddPunctuation(ekwk_D_links);
+        AddPunctuation(eredivisie_links);
+        AddPunctuation(europacup_links);
+
+        MultipleStrings content;
+        content.addContent("<ol> <li>Algemeen: <a href=\"../pages//index.html\">Home page</a> </li>");
+        content.addContent("<ol> <li><a href=\"klaverjas_faq.html\">een JavaScript-spel: klaverjassen</a></li>");
+        content.addContent("<li><a href=\"anybrowser.html\">speciale characters in html</a> en <a href=\"tmp_ascii_codes.html\">ascii codes</a></li>");
+        content.addContent("<li><a href=\"cv.html\">mijn CV</a></li>");
+        content.addContent("<li><a href=\"samenvatting_proefschrift.html\">samenvatting van mijn proefschrift</a></li>");
+        content.addContent("<li><a href=\"reactie.html\">verzoek om reacties, opmerkingen op deze site</a></li>");
+        content.addContent("<li>Technische documentatie: <a href=\"tech_doc_kj.html\">klaverjassen</a> en <a href=\"tech_doc_shob.html\">shob</a>.</li> </ol>");
+
+        content.addContent("<li>Sport: <a href=\"sport.html\">sportpagina's</a></li> ");
+        content.addContent("<ol> <li>Voetbal: <a href=\"sport_voetbal_nl_stats.html\">statistieken eredivisie</a> en <a href=\"sport_voetbal_nl_stats_more.html\">nog meer stats</a> </li>");
+        content.addContent("<li>Nederlands betaald voetbal, seizoenen:<br> ");
+        content.addContentReversed(eredivisie_links);
+        content.addContent("</li>");
+        content.addContent(" <li>Europacup voetbal, seizoenen:<br>");
+        content.addContentReversed(europacup_links);
+        content.addContent("</li>");
+        content.addContent(" <li>schaatsuitslagen Olympische Winterspelen:<br>");
+        content.addContentReversed(os_links);
+        content.addContent("</li>");
+        content.addContent(" <li>Wedstrijden Nederlands mannenelftal:<br>");
+        content.addContentReversed(ekwk_links);
+        content.addContent("</li>");
+        content.addContent(" <li>Wedstrijden Nederlands vrouwenelftal:<br>");
+        content.addContentReversed(ekwk_D_links);
+        content.addContent("</li> </ol>");
+
+        content.addContent("<li>Bookmarks: <ol> <li> <a href=\"bookmarks_sport.html\">sport</a>, ");
+        content.addContent("<a href=\"bookmarks_treinen.html\">treinen</a>, ");
+        content.addContent("<a href=\"bookmarks_media.html\">media</a> en ");
+        content.addContent("<a href=\"bookmarks_computers.html\">computers en internet</a>.</li> </ol> </ol>");
+
+        auto hb = HeadBottomInput(last_dd);
+        hb.title = "Overzicht van de website van Edwin Spee";
+        hb.css = StyleSheetType::SeparateFile;
+        hb.copyTitleToH1 = false;
+        hb.body = table::yellowRed(content, hb.title);
+        auto pageContent = HeadBottom::getPage(hb);
+
+        updateIfDifferent::update("../pages/overzicht.html", pageContent);
     }
 }
--- src/shob.pages/FormatBookmarks.h
+++ src/shob.pages/FormatBookmarks.h
@@ -10,9 +10,11 @@ namespace shob::pages
         FormatBookmarks(const std::string& folder, const int dd);
         void rebuildBookmarks(const std::string& page, const bool is_tmp = false) const;
         static void rebuildOwnSportLinks();
+        static void rebuildOverzicht();
     private:
         bookmarks::ConfigurableBookmarks bookmarks;
         int dd;
+        static constexpr int last_dd = 20260912;
         general::MultipleStrings currentEventsLinks() const;
         static void AddPunctuation(general::MultipleStrings& extra);
     };
